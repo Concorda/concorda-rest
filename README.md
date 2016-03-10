56f92d371e8bb365e74ae65d9433a62db74a46f6
malex <mircea.alexandru@gmail.com>
2016-03-10 12:12:26 +0200

    moved implementation form concorda-dashboard

---
+++ README.md
@@ -0,0 +1,28 @@
+![Banner][]
+
+# Concorda Dashboard REST API
+Concorda: User management system
+
+- __Lead Maintainer:__ [Mircea Alexandru][lead]
+- __Sponsor:__ [nearForm][]
+
+
+A detailed documentation can be found in [Concorda wiki](https://github.com/Concorda/docs/blob/master/Readme.md)
+
+
+## Contributing
+The [Concorda][] encourages open participation. If you feel you can help in any way, be it with
+documentation, examples, extra testing, or new features please get in touch.
+
+- [Code of Conduct]
+
+## License
+Copyright (c) 2016, nearForm and other contributors.
+Licensed under [MIT][].
+
+[Banner]: https://raw.githubusercontent.com/concorda/concorda-dashboard/master/public/client/assets/img/logo-concorda-banner.png
+[MIT]: ./LICENSE
+[Code of Conduct]: https://github.com/concorda/concorda-dashboard/CoC.md
+[Concorda]: https://github.com/concorda/concorda-dashboard
+[lead]: https://github.com/mirceaalexandru
+[nearForm]: http://www.nearform.com/
---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var Hapi = require('hapi')
+var Bell = require('bell')
+var Chairo = require('chairo')
+var Cookie = require('hapi-auth-cookie')
+var Nes = require('nes')
+var Frontend = require('./frontend')
+var Backend = require('./backend')
+var Good = require('good')
+var GoodConsole = require('good-console')
+
+var Services = require('./lib/services');
+
+// Options for our hapi plugins.
+var opts = {
+  server: {
+    port: Number(process.env.SERVICE_PORT || 3030),
+    host: process.env.SERVICE_HOST
+  },
+  chairo: {
+    timeout: 2000,
+    secure: true,
+    log: 'level:info'
+  },
+  good: {
+    opsInterval: 1000,
+    reporters: [{reporter: GoodConsole, events: {log: '*', response: '*'}}]
+  }
+}
+
+// Create our server.
+var server = new Hapi.Server({ debug: { request: ['error'] } })
+server.connection(
+  {
+    host: opts.server.host,
+    port: opts.server.port
+  }
+)
+
+// Declare our Hapi plugin list.
+var plugins = [
+  {register: Bell},
+  {register: Cookie},
+  {register: Chairo, options: opts.chairo},
+  {register: Nes},
+  {register: Good, options: opts.good},
+  {register: Services}
+]
+
+server.register(plugins,
+function (err) {
+  if (err) {
+    throw err;
+  }
+  server.start(function () {
+    console.log('listening on port: ' + process.env.SERVICE_PORT);
+  });
+});
